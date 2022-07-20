0def2095347191a33422e1ec55bc449d05e98063
aleksCheh <chekryzhov.aleks@gmail.com>
2022-07-21 01:54:44 +0300

    Formatted spawner

--- src/dungeon_crawler/spawner.rs
+++ src/dungeon_crawler/spawner.rs
@@ -1,9 +1,11 @@
 pub use crate::dungeon_crawler::prelude::*;
 
-pub fn goblin() -> (i32, String, FontCharType) {
+pub fn goblin() -> (i32, String, FontCharType)
+{
     (10, "Goblin".to_string(), to_cp437('g'))
 }
-pub fn orc() -> (i32, String, FontCharType) {
+pub fn orc() -> (i32, String, FontCharType)
+{
     (30, "Orc".to_string(), to_cp437('o'))
 }
 pub struct Spawner {}
@@ -15,15 +17,12 @@ pub fn spawn_player(ecs: &mut World, pos: Point) {
             color: ColorPair::new(WHITE, BLACK),
             glyph: to_cp437('@'),
         },
-        Health {
-            max: 20,
-            current: 20,
-        },
+        Health {max: 20, current: 20}
     ));
 }
 
 pub fn spawn_enemy(ecs: &mut World, rng: &mut RandomNumberGenerator, pos: Point) {
-    let (hp, name, glyph) = match rng.roll_dice(1, 10) {
+    let (hp, name, glyph) =  match rng.roll_dice(1, 10) {
         1..=8 => goblin(),
         _ => orc(),
     };
@@ -32,13 +31,10 @@ pub fn spawn_enemy(ecs: &mut World, rng: &mut RandomNumberGenerator, pos: Point)
         pos,
         Render {
             color: ColorPair::new(WHITE, BLACK),
-            glyph: glyph,
+            glyph: glyph,          
         },
         MovingRandomly,
-        Health {
-            max: hp,
-            current: hp,
-        },
+        Health{max: hp, current: hp},
         Name(name),
     ));
 }
